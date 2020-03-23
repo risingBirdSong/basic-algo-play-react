e54c31da567593eb4cc6a21378644751a7724816
risingbirdsong <heregoingthere@outlook.com>
2020-03-22 22:30:46 -0700

    initial commit, working number array tracker ... displays current number

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,70 @@
-import React from 'react';
+import React, { useState, useEffect, Fragment } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+type strOrNumberOrBoolean = string | number | boolean;
+
 function App() {
+
+  let [numbers, setNumbers] = useState<strOrNumberOrBoolean[]>([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+  let [curIndex, setCurIndex] = useState(0);
+  // let [currentNumber, setCurrentNumber] = useState(curIndex);
+
+  const inc = () => {
+    if (curIndex < numbers.length) {
+      setCurIndex(curIndex + 1);
+    }
+    else {
+      console.log("done iterating");
+    }
+  }
+
+  const dec = () => {
+    if (curIndex > 0) {
+      setCurIndex(curIndex - 1);
+    }
+    else {
+      console.log("done iterating backwards");
+    }
+  }
+
+
+  function iterationLogic(arr: strOrNumberOrBoolean[]) {
+    return (
+      arr.map((x, idx, theArr) => {
+        if (idx < arr.length - 1) {
+          // if (typeof arr[0] === "number") {
+          if (x === numbers[curIndex]) {
+            return <React.Fragment> <span className="highlight">{x} </span></React.Fragment>
+          }
+          else {
+            return `${x} , `;
+          }
+        }
+        else {
+          if (x === numbers[curIndex]) {
+            return <span className="highlight">{`${x}`}</span>
+          }
+          else {
+            return x;
+          }
+        }
+        // }
+      })
+    )
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="buttons">
+          <button className="inc" onClick={inc}>inc</button>
+          <button className="dec" onClick={dec}>dec</button>
+        </div>
+        {curIndex < numbers.length ? "" : <h1>all done iterating!</h1>}
+        <h2 className="currentNum">{numbers[curIndex]}</h2>
+        <h3> numbers : [{iterationLogic(numbers)}]</h3>
+      </div>
     </div>
   );
 }
